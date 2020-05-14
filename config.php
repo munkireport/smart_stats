3f66ee59e7874f90465d312c0cbc01c815f060cb
tuxudo <tuxudo@users.noreply.github.com>
2020-05-14 12:55:53 -0400

    Fixed config.php

--- config.php
+++ config.php
@@ -1,17 +1,16 @@
 <?php
 return [
-	/*
-	|===============================================
-	| smart_stats
-	|===============================================
-	|
-	| By default, smart_stats can keeps historical
-	| drives that have been logged in the past. To
-	| disable, set `keep_smart_stats_historical` to
-	| false.
-	|
-	*/
 
-//	$conf['keep_smart_stats_historical'] => env('keep_smart_stats_historical', true),
-    'keep_smart_stats_historical' = true,
+    |===============================================
+    | smart_stats
+    |===============================================
+    |
+    | By default, smart_stats can keeps historical
+    | drives that have been logged in the past. To
+    | disable, set `keep_smart_stats_historical` to
+    | false.
+    |
+    */
+
+    'keep_smart_stats_historical' = env('KEEP_SMART_STATS_HISTORICAL', true),
 ];
